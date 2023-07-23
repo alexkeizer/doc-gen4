128d6238543aad7fcdc09b1912a13107cddb0a24
Alex Keizer <alex@keizer.dev>
2023-07-23 16:56:59 +0100

    feat: delay search a little bit, so that UI thread doesn't hang while typing

--- static/search.js
+++ static/search.js
@@ -78,10 +78,22 @@ function removeAllChildren(node) {
 }
 
 /**
+ * Only start searching 500ms after the last input event
+ */
+function searchAfterDelay(dataCenter, err, inputElement, sr, maxResults, autoComplete) {
+  clearTimeout(inputElement.searchTimeout)
+
+  inputElement.searchTimeout = setTimeout(
+    () => handleSearch(dataCenter, err, inputElement, sr, maxResults, autoComplete),
+    500
+  )
+}
+
+/**
  * Handle user input and perform search.
  */
-function handleSearch(dataCenter, err, ev, sr, maxResults, autocomplete) {
-  const text = ev.target.value;
+function handleSearch(dataCenter, err, inputElement, sr, maxResults, autocomplete) {
+  const text = inputElement.value;
 
   // If no input clear all.
   if (!text) {
@@ -108,7 +120,7 @@ function handleSearch(dataCenter, err, ev, sr, maxResults, autocomplete) {
     const result = dataCenter.search(text, false, allowedKinds, maxResults);
 
     // in case user has updated the input.
-    if (ev.target.value != text) return;
+    if (inputElement.value != text) return;
   
     // update autocomplete results
     removeAllChildren(sr);
@@ -140,9 +152,13 @@ function handleSearch(dataCenter, err, ev, sr, maxResults, autocomplete) {
 DeclarationDataCenter.init()
   .then((dataCenter) => {
     // Search autocompletion.
-    SEARCH_INPUT.addEventListener("input", ev => handleSearch(dataCenter, null, ev, ac_results, AC_MAX_RESULTS, true));
+    SEARCH_INPUT.addEventListener("input", ev => 
+      searchAfterDelay(dataCenter, null, ev.target, ac_results, AC_MAX_RESULTS, true)
+    );
     if(SEARCH_PAGE_INPUT) {
-      SEARCH_PAGE_INPUT.addEventListener("input", ev => handleSearch(dataCenter, null, ev, SEARCH_RESULTS, SEARCH_PAGE_MAX_RESULTS, false))
+      SEARCH_PAGE_INPUT.addEventListener("input", ev => 
+        searchAfterDelay(dataCenter, null, ev.target, SEARCH_RESULTS, SEARCH_PAGE_MAX_RESULTS, false)
+      );
       document.querySelectorAll(".kind_checkbox").forEach((checkbox) =>
         checkbox.addEventListener("input", ev => SEARCH_PAGE_INPUT.dispatchEvent(new Event("input")))
       );
@@ -151,8 +167,12 @@ DeclarationDataCenter.init()
     SEARCH_INPUT.dispatchEvent(new Event("input"))
   })
   .catch(e => {
-    SEARCH_INPUT.addEventListener("input", ev => handleSearch(null, e, ev, ac_results, AC_MAX_RESULTS,true ));
+    SEARCH_INPUT.addEventListener("input", ev => 
+      searchAfterDelay(null, e, ev.target, ac_results, AC_MAX_RESULTS,true )
+    );
     if(SEARCH_PAGE_INPUT) {
-      SEARCH_PAGE_INPUT.addEventListener("input", ev => handleSearch(null, e, ev, SEARCH_RESULTS, SEARCH_PAGE_MAX_RESULTS, false));
+      SEARCH_PAGE_INPUT.addEventListener("input", ev => 
+        searchAfterDelay(null, e, ev.target, SEARCH_RESULTS, SEARCH_PAGE_MAX_RESULTS, false)
+      );
     }
   });
